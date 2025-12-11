325fcc86fbd03b413db641a00a99855a67ac786a
Deivith <deivithfagundes@gmail.com>
2025-12-10 21:20:11 -0300

    fetch: html da area de busca do header

--- src/Components/HeaderMain/index.tsx
+++ src/Components/HeaderMain/index.tsx
@@ -2,12 +2,25 @@ import MenuHamburguer from '../MenuHamburguer'
 
 import logo from '../../Assets/images/logo-desktop.svg'
 
+import { CiSearch } from 'react-icons/ci'
+
 import * as S from './styles'
+import React from 'react'
 
 const HeaderMain = () => (
   <S.MainHeader>
     <img src={logo} alt="Zaffari Bourbon" />
     <MenuHamburguer />
+    <S.Search>
+      <input
+        type="text"
+        id="downshift"
+        placeholder="Pesquise produtos ou marcas"
+      />
+      <button>
+        <CiSearch size={16} />
+      </button>
+    </S.Search>
   </S.MainHeader>
 )
 
--- src/Components/HeaderMain/styles.ts
+++ src/Components/HeaderMain/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { colors } from '../../GlobalStyles'
 
 export const MainHeader = styled.div`
   display: flex;
@@ -8,5 +9,32 @@ export const MainHeader = styled.div`
   img {
     height: 74px;
     width: 208px;
+  }
+`
+
+export const Search = styled.div`
+  border-radius: 8px;
+  background-color: ${colors.lightGray};
+  display: flex;
+
+  input {
+    height: 48px;
+    background-color: transparent !important;
+    border-radius: 0;
+    border: none;
+    color: ${colors.gray};
+    font-family: segoeui, sans-serif;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400px;
+    padding: 0 0 0 16px;
+    line-height: 1.5;
+    width: 798px;
+  }
+
+  button {
+    height: 48px;
+    width: 48px;
+    border: none;
   }
 `
